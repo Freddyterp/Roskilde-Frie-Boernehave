39d18513655513b27276098938eecee90ca5be6f
Freddyterp <frederik.terp1@gmail.com>
2020-03-25 09:52:34 +0100

    Update Files.java

--- src/Files.java
+++ src/Files.java
@@ -1,5 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-class Files {
+class Files
+{
+
+    // Create new file
+    private static File database = new File("workHours.dat");
+    private String filename = database.getName();
+    Input in = new Input();
+
+    public Files(ArrayList<Teacher> arr)
+    {
+        try
+        {
+            Scanner read = new Scanner(new File(filename));
+
+            while(read.hasNext())
+            {
+                int ID = read.nextInt();
+                int weekNr = read.nextInt();
+                String weekDay = read.next();
+                int startTime = read.nextInt();
+                int endTime = read.nextInt();
+
+                for(Teacher teacher : arr)
+                {
+                    if(teacher.getID() = ID)
+                    {
+
+                    }
+                }
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
